61bb81cc1ed904f63663e8f2a9e34c0b24bff0c5
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-09 17:56:51 -0500

    Basic outline for the phonebook

---
+++ src/phonebook/components/PhoneBookList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Panel from 'react-bootstrap/lib/Panel';
+
+class PhoneBookList extends React.Component {
+
+  /**
+   * @constructor
+   */
+  constructor(options) {
+    super(options);
+  }
+
+  render () {
+    return <Panel>
+            <p>entries!</p>
+           </Panel>;
+  }
+
+}
+
+export default PhoneBookList;
--- src/phonebook/phonebook.js
+++ src/phonebook/phonebook.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Panel from 'react-bootstrap/lib/Panel';
+
+import PhoneBookList from './components/PhoneBookList';
 
 /*
  * @class Humble beginnings for the phone book
@@ -18,6 +24,18 @@ class PhoneBook extends React.Component {
             <Jumbotron>
               <h1>Phonebook</h1>
             </Jumbotron>
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={4} md={3}>
+                    <PhoneBookList />
+                </Col>
+                <Col xs={16} md={8}>
+                    <Panel>
+                        <p>Something will need to go in here!</p>
+                    </Panel>
+                </Col>
+              </Row>
+            </Grid>
            </div>;
   }
 
