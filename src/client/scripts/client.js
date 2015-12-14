d5acd66cc56de4587ab1a50adffdc3696760c56f
Matt Uttridge <matt.uttridge@willowtreeapps.com>
2015-12-14 16:34:30 -0500

    Use the library provider impl

--- src/client/scripts/client.js
+++ src/client/scripts/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PhoneBook from '../../phonebook';
-import Provider from '../../phonebook/components/Provider';
+import { Provider } from 'react-redux';
 import store from '../../phonebook/store/store';
 
 store.dispatch({
